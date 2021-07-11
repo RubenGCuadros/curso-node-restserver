e678016668a959a652587de940cb668ca052abc9
RGCuadros <ruben.garcia@grupoonetec.com>
2021-07-11 13:33:44 +0200

    Google sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -51,8 +52,60 @@ const login = async( req, res = response ) => {
 
 }
 
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+    
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        })
+
+    }
+
+    
+
+}
+
 
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
